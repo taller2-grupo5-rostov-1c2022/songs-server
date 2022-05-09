abccef058e0dcc65d7c604ae80b9f1423a86b9e1
nicolas_zulaica <nzulaica@fi.uba.ar>
2022-05-08 23:37:12 -0300

    no credentials when testing

--- src/firebase/access.py
+++ src/firebase/access.py
@@ -1,5 +1,6 @@
 import firebase_admin
 import json
+from src.constants import TESTING
 from firebase_admin import credentials
 from firebase_admin import storage
 
@@ -7,16 +8,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Use a service account
-# Si tira error porque no encuentra el archivo, copiar el google-credentials.json a /src
-with open("/code/src/google-credentials.json") as json_file:
-    cert_dict = json.load(json_file, strict=False)
+if not TESTING:
+    # Use a service account
+    # Si tira error porque no encuentra el archivo, copiar el google-credentials.json a /src
+    with open("/code/src/google-credentials.json") as json_file:
+        cert_dict = json.load(json_file, strict=False)
 
-cred = credentials.Certificate(cert_dict)
+    cred = credentials.Certificate(cert_dict)
 
-firebase_admin.initialize_app(cred, {"storageBucket": "rostov-spotifiuby.appspot.com/"})
+    firebase_admin.initialize_app(
+        cred, {"storageBucket": "rostov-spotifiuby.appspot.com/"}
+    )
 
-bucket = storage.bucket("rostov-spotifiuby.appspot.com")
+    bucket = storage.bucket("rostov-spotifiuby.appspot.com")
 
 
 def get_bucket():
