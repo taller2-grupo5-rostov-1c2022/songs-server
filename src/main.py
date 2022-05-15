4fbf32a87142d435d68c9849b7e661c7a8c39970
nicolas_zulaica <nzulaica@fi.uba.ar>
2022-05-15 19:24:45 -0300

    version

--- src/main.py
+++ src/main.py
@@ -7,7 +7,12 @@ from src.middleware.utils import get_api_key
 
 API_VERSION_PREFIX = "/api/v3"
 
-app = FastAPI(dependencies=[Depends(get_api_key)])
+app = FastAPI(
+    title="Songs API",
+    description="Spotifiuby's API to manage songs, albums, playlists and users",
+    version="3.3.3",
+    dependencies=[Depends(get_api_key)],
+)
 
 app.include_router(songs.router, prefix=API_VERSION_PREFIX)
 app.include_router(albums.router, prefix=API_VERSION_PREFIX)
