72e747bf13854328b4751665045edf0a63c950ff
efoppiano <efoppiano@fi.uba.ar>
2022-06-19 17:32:45 -0300

    Add more pagination tests

--- tests/test_paginations.py
+++ tests/test_paginations.py
@@ -324,3 +324,193 @@ def test_get_songs_second_page(client, custom_requests_mock):
     assert response_get.status_code == 200
     assert len(songs) == 1
     assert songs[0]["name"] == "song_2"
+
+
+def test_get_songs_filtered_by_name_first_page(client, custom_requests_mock):
+    utils.post_user(client, "user_id", "user_name")
+
+    utils.post_song(client, "user_id", "foo")
+    utils.post_song(client, "user_id", "song_1")
+    utils.post_song(client, "user_id", "song_2")
+
+    response_get = client.get(
+        f"{API_VERSION_PREFIX}/songs/",
+        params={"page": 0, "size": 1, "name": "song"},
+        headers={"api_key": "key", "uid": "user_id"},
+        with_pagination=True,
+    )
+
+    songs = response_get.json()
+    assert response_get.status_code == 200
+    assert len(songs) == 1
+    assert songs[0]["name"] == "song_1"
+
+
+def test_get_songs_filtered_by_name_second_page(client, custom_requests_mock):
+    utils.post_user(client, "user_id", "user_name")
+
+    utils.post_song(client, "user_id", "foo")
+    utils.post_song(client, "user_id", "song_1")
+    utils.post_song(client, "user_id", "song_2")
+
+    response_get = client.get(
+        f"{API_VERSION_PREFIX}/songs/",
+        params={"page": 1, "size": 1, "name": "song"},
+        headers={"api_key": "key", "uid": "user_id"},
+        with_pagination=True,
+    )
+
+    songs = response_get.json()
+    assert response_get.status_code == 200
+    assert len(songs) == 1
+    assert songs[0]["name"] == "song_2"
+
+
+def test_get_albums_filtered_by_artist_first_page(client, custom_requests_mock):
+    utils.post_user(client, "user_id", "user_name")
+
+    song_id_1 = utils.post_song(client, "user_id", "song_1", artists=["foo"]).json()[
+        "id"
+    ]
+    song_id_2 = utils.post_song(
+        client, "user_id", "song_2", artists=["artist_1"]
+    ).json()["id"]
+    song_id_3 = utils.post_song(
+        client, "user_id", "song_2", artists=["artist_1"]
+    ).json()["id"]
+
+    utils.post_album(client, "user_id", "album_1", songs_ids=[song_id_1, song_id_2])
+    utils.post_album(client, "user_id", "album_2", songs_ids=[song_id_3])
+
+    response_get = client.get(
+        f"{API_VERSION_PREFIX}/albums/",
+        params={"page": 0, "size": 1, "artist": "artist_1"},
+        headers={"api_key": "key", "uid": "user_id"},
+        with_pagination=True,
+    )
+
+    albums = response_get.json()
+    assert response_get.status_code == 200
+    assert len(albums) == 1
+    assert albums[0]["name"] == "album_1"
+
+
+def test_get_albums_filtered_by_artist_second_page(client, custom_requests_mock):
+    utils.post_user(client, "user_id", "user_name")
+
+    song_id_1 = utils.post_song(client, "user_id", "song_1", artists=["foo"]).json()[
+        "id"
+    ]
+    song_id_2 = utils.post_song(
+        client, "user_id", "song_2", artists=["artist_1"]
+    ).json()["id"]
+    song_id_3 = utils.post_song(
+        client, "user_id", "song_2", artists=["artist_1"]
+    ).json()["id"]
+
+    utils.post_album(client, "user_id", "album_1", songs_ids=[song_id_1, song_id_2])
+    utils.post_album(client, "user_id", "album_2", songs_ids=[song_id_3])
+
+    response_get = client.get(
+        f"{API_VERSION_PREFIX}/albums/",
+        params={"page": 1, "size": 1, "artist": "artist_1"},
+        headers={"api_key": "key", "uid": "user_id"},
+        with_pagination=True,
+    )
+
+    albums = response_get.json()
+    assert response_get.status_code == 200
+    assert len(albums) == 1
+    assert albums[0]["name"] == "album_2"
+
+
+def test_get_playlists_filtered_by_colab_first_page(client, custom_requests_mock):
+    utils.post_user(client, "user_playlist_owner", "Ricardito")
+    utils.post_user(client, "user_playlist_colab", "Fernandito")
+    utils.post_user(client, "foo_id", "foo_name")
+
+    utils.post_playlist(client, "user_playlist_owner", "playlist_1")
+    utils.post_playlist(
+        client, "user_playlist_owner", "playlist_2", colabs_ids=["user_playlist_colab"]
+    )
+    utils.post_playlist(
+        client, "user_playlist_owner", "playlist_3", colabs_ids=["user_playlist_colab"]
+    )
+
+    response_get = client.get(
+        f"{API_VERSION_PREFIX}/playlists/",
+        params={"page": 0, "size": 1, "colab": "user_playlist_colab"},
+        headers={"api_key": "key", "uid": "user_playlist_owner"},
+        with_pagination=True,
+    )
+
+    playlists = response_get.json()
+    assert response_get.status_code == 200
+    assert len(playlists) == 1
+    assert playlists[0]["name"] == "playlist_2"
+
+
+def test_get_playlists_filtered_by_colab_second_page(client, custom_requests_mock):
+    utils.post_user(client, "user_playlist_owner", "Ricardito")
+    utils.post_user(client, "user_playlist_colab", "Fernandito")
+    utils.post_user(client, "foo_id", "foo_name")
+
+    utils.post_playlist(client, "user_playlist_owner", "playlist_1")
+    utils.post_playlist(
+        client, "user_playlist_owner", "playlist_2", colabs_ids=["user_playlist_colab"]
+    )
+    utils.post_playlist(
+        client, "user_playlist_owner", "playlist_3", colabs_ids=["user_playlist_colab"]
+    )
+
+    response_get = client.get(
+        f"{API_VERSION_PREFIX}/playlists/",
+        params={"page": 1, "size": 1, "colab": "user_playlist_colab"},
+        headers={"api_key": "key", "uid": "user_playlist_owner"},
+        with_pagination=True,
+    )
+
+    playlists = response_get.json()
+    assert response_get.status_code == 200
+    assert len(playlists) == 1
+    assert playlists[0]["name"] == "playlist_3"
+
+
+def test_get_songs_with_blocked_songs_first_page(client, custom_requests_mock):
+    utils.post_user(client, "user_id", "user_name")
+
+    utils.post_song(client, "user_id", "song_1")
+    utils.post_song(client, "user_id", "song_2", blocked=True)
+    utils.post_song(client, "user_id", "song_3")
+
+    response_get = client.get(
+        f"{API_VERSION_PREFIX}/songs/",
+        params={"page": 0, "size": 1},
+        headers={"api_key": "key", "uid": "user_id"},
+        with_pagination=True,
+    )
+
+    songs = response_get.json()
+    assert response_get.status_code == 200
+    assert len(songs) == 1
+    assert songs[0]["name"] == "song_1"
+
+
+def test_get_songs_with_blocked_songs_second_page(client, custom_requests_mock):
+    utils.post_user(client, "user_id", "user_name")
+
+    utils.post_song(client, "user_id", "song_1")
+    utils.post_song(client, "user_id", "song_2", blocked=True)
+    utils.post_song(client, "user_id", "song_3")
+
+    response_get = client.get(
+        f"{API_VERSION_PREFIX}/songs/",
+        params={"page": 1, "size": 1},
+        headers={"api_key": "key", "uid": "user_id"},
+        with_pagination=True,
+    )
+
+    songs = response_get.json()
+    assert response_get.status_code == 200
+    assert len(songs) == 1
+    assert songs[0]["name"] == "song_3"
